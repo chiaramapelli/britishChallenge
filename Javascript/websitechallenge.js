c537944c910994dc6ef81860d9f11ce2bf1dece2
Code Nation 109 <codenation109@CN0109.local>
2019-05-03 14:23:21 +0100

    britishChallenge

---
+++ Javascript/websitechallenge.js
@@ -0,0 +1,5 @@
+document.getElementById("myButton").addEventListener("click", function() {
+    document.getElementById("wholepage").style.display = "none"
+    alert('You are so British!')
+   
+});
